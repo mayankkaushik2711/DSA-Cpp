2df3333173e5d1115892b1a158aa3a691d3351c4
mayank <iammayank2711@gmail.com>
2023-12-03 23:55:07 +0530

    first commit

---
+++ IsPow2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=64;
+    if (a & (a-1) == 0)
+    {
+        cout<<"pow of 2";
+    }
+    else{
+        cout<<"fkoff";
+    }
+
+}
+
+//maxy boy
+//maxy boy
